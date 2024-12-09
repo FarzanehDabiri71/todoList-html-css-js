bce89203d320a9bf85aaeb220b5b2cb97bf0f625
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-09 23:14:02 +0330

    Fix addTodo function

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -6,3 +6,8 @@ for (let i = 0; i < todoList.length; i++) {
   todoListHTML += html;
 }
 document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+function addTodo() {
+  const inputElement = document.querySelector("js-name-input");
+  const name = inputElement.value;
+  todoList.push(name);
+}
