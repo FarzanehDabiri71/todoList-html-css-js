ef50ea0b9a2d441ec3332c12350540fabbe7dd53
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-10 22:45:55 +0330

    Implemented delete button

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -6,7 +6,15 @@ function renderTodoList() {
   let todoListHTML = "";
   for (let i = 0; i < todoList.length; i++) {
     const todo = todoList[i];
-    const html = `<p> ${todo} </p>`;
+    const html = `
+      <p> 
+        ${todo}
+         <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList()
+         " > Delete </button>
+      </p>
+     `;
     todoListHTML += html;
   }
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
