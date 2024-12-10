bfb496a6711704c48719a747458f31bd568fe55c
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-10 23:00:27 +0330

    Add date input functionality

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,14 +1,20 @@
-const todoList = ["make dinner", "wash dishes"];
+const todoList = [
+  { name: "make dinner", dueDate: "2022-12-22" },
+  { name: "wash dishes", dueDate: "2022-12-22" },
+];
 
 renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = "";
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
     const html = `
       <p> 
-        ${todo}
+        ${name} ${dueDate}
          <button onclick="
           todoList.splice(${i}, 1);
           renderTodoList()
@@ -22,8 +28,11 @@ function renderTodoList() {
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
+  const dateInputElement = document.querySelector(".js-due-date-input");
   const name = inputElement.value;
-  todoList.push(name);
+  const dueDate = dateInputElement.value;
+
+  todoList.push({ name, dueDate });
   inputElement.value = "";
   renderTodoList();
 }
